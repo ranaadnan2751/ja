df208b039e5fc4e4b17e02ae32c8679b8b51c563
ranaadnan2751 <64990187+ranaadnan2751@users.noreply.github.com>
2020-06-15 20:19:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,289 @@
+// ==============> chapter # 1 task # 1 <===================
+
+
+
+// alert("welcome to my website");
+
+
+
+// ==============> chapter # 1 task # 2 <===================
+
+
+// alert("Error! Please enter a valid password.");
+
+
+
+// ==============> chapter # 1 task # 3 <===================
+
+
+
+
+// alert("Welcome to JS Land \n Happy Coading!");
+
+
+
+
+// ==============> chapter # 1 task # 4 <===================
+
+
+
+// alert("Welcome to JS Land");
+// alert("Happy Coading!");
+
+
+
+// ==============> chapter # 1 task # 5 <===================
+
+
+
+// console.log("i can run js through my web browser's console")
+
+
+
+// ==============> chapter # 1 task # 6 <===================
+
+
+// alert("this is my website")
+
+
+
+// ==============> chapter # 1 task # 7 <===================
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <script src="app.js"></script>
+// </head>
+// <body>
+    
+    
+    
+    
+    
+    
+//     <script> 
+
+//     </script>  
+// </body>
+// </html>
+
+
+
+
+// ==============> chapter # 2 task # 1 <===================
+
+
+// var username = ;
+
+
+// ==============> chapter # 2 task # 2 <===================
+
+
+// var myName = "rana muhammad adnan";
+
+
+
+
+// ==============> chapter # 2 task # 3 <===================
+
+
+// var massage = "Hello World";
+
+// alert(massage)
+
+
+
+
+// ==============> chapter # 2 task # 4 <===================
+
+
+
+// var name = "jhone Doe";
+// var age =  "15 years old";
+// var course = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+
+
+
+
+// ==============> chapter # 2 task # 5 <===================
+
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+// alert(pizza)
+
+
+
+
+
+// ==============> chapter # 2 task # 6 <===================
+
+
+// var email = "ranaadnan2751@gmail.com";
+
+// alert(" my email address is " + email);
+
+
+
+
+
+// ==============> chapter # 2 task # 7 <===================
+
+
+// var book = " A Smarter way to learn JavaScript ";
+
+// alert(" I am trying to learn from the Book " + book)
+
+
+
+// ==============> chapter # 2 task # 8 <===================
+
+
+// document.write(" Yahh! I can write Content through JavaScript ")
+
+
+
+
+
+// ==============> chapter # 2 task # 9 <===================
+
+
+// var design = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+
+// alert(design);
+
+// document.write(design);
+
+
+
+
+
+// ==============> chapter # 3 task # 1 <===================
+
+
+
+
+// var age = 38;
+
+// alert(" I am " + age + " Years Old")
+
+
+
+
+
+// ==============> chapter # 3 task # 2 <===================
+
+
+
+// var visits = 14;
+
+// alert("You have visited this site " + visits + " times")
+
+
+
+
+
+
+
+// ==============> chapter # 3 task # 3 <===================
+
+
+// var birthYear = 1982;
+
+// document.write("My birth Year is " + birthYear)
+// document.write("<br> Data type of my declaired variable is number ")
+
+
+
+
+
+
+
+// ==============> chapter # 3 task # 4 <===================
+
+
+
+
+// var customerName = prompt("enter your name");
+// var productQuentity = prompt("Enter Quentity of Products you want to Order") 
+// var productTitle = prompt("enter procut's Name")
+
+// document.write(customerName + " Orderd " + productQuentity +" " + productTitle + " From XYZ Clothing Store ")
+
+
+
+
+
+
+
+
+// ==============> chapter # 4 task # 1 <===================
+
+
+// var name1 = "adnan", name2 = "ali", name3 = "imran"; 
+
+
+
+
+
+
+// ==============> chapter # 4 task # 2 <===================
+
+// legal variables
+
+// var studentName =;
+// var student1 = ;
+// var _student = ;
+// var $student = ;
+// var studentname = ;
+
+// illigal variables
+
+// var 1student = ; 
+// var @student = ;
+// var student name = ;
+// var student-name = ;
+// var let_student = ;
+
+
+
+
+
+
+
+// ==============> chapter # 4 task # 3 <===================
+
+
+
+
+
+// document.write("<strong>Rules for naming JS variables</strong><br><br>" +
+// " Variable names can only contain , Numbers, $ and _ . For example : $my_firstVariable <br>" +
+// " Variables must begin with a letter, $ or _ For example $name, _name or name <br>" +
+// " Variable names are case sensitive <br>" +
+// " Variable names should not be JS Keywords"
+
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
